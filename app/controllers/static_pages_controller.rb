b5b96cd7d2aa23179c6287fc992e56994f45cd65
balaji.majji <balaji.majji@chronus.com>
2014-08-13 20:31:33 +0530

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+  	@micropost = current_user.microposts.build if signed_in?
+  	@microposts = current_user.microposts.paginate(page: params[:page])
+  	@feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   def help
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,5 +43,10 @@ module SessionsHelper
   def store_location
     session[:return_to] = request.url if request.get?
   end
+
+  def signed_in_user
+  	  store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
   
 end
